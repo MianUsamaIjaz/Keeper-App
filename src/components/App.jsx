8af27ec46de9abf343f523f2935b5d74daa77422
Mian Usama Ijaz <mianusama9090@gmail.com>
2023-07-27 04:16:28 -0230

    Final Phase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,28 +5,40 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
 
-    const [notes, setNotes] = useState([]);
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
 
-    function addNote(note) {
-        setNotes(prevNotes => {
-            return [...prevNotes, note];
-        })
-    }
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
 
-    return (
-        <div>
-        <Header />
-        <CreateArea onAdd={addNote} />
-        {notes.map((noteItem) => {
-            return <Note 
-            title = {noteItem.title}
-            content = {noteItem.content}
-            />
-        })}
-        <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
